e0a77e0d00e46c65326cf805271eb6b00e68f80d
Adam P. Lally <alally@apache.org>
2006-11-15 20:07:44 +0000

    JIRA issue UIMA-26:  changed C:\Program Files\IBM\uima to C:\Program Files\apache\uima
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@475388 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-distr/src/main/scripts/adjustExamplePaths.sh
+++ uimaj-distr/src/main/scripts/adjustExamplePaths.sh
@@ -3,8 +3,8 @@ if [ "$JAVA_HOME" = "" ];
 then
   JAVA_HOME=$UIMA_HOME/java/jre
 fi
-"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/docs/examples" .xml "C:/Program Files/IBM/uima" "$UIMA_HOME" -ignorecase
-"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/docs/examples" .classpath "C:/Program Files/IBM/uima" "$UIMA_HOME" -ignorecase
-"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/docs/examples" .launch "C:/Program Files/IBM/uima" "$UIMA_HOME" -ignorecase
-"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/docs/examples" .wsdd "C:/Program Files/IBM/uima" "$UIMA_HOME" -ignorecase
-"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/docs/examples" .xml "C:/Temp" temp -ignorecase
+"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/examples" .xml "C:/Program Files/apache/uima" "$UIMA_HOME" -ignorecase
+"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/examples" .classpath "C:/Program Files/apache/uima" "$UIMA_HOME" -ignorecase
+"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/examples" .launch "C:/Program Files/apache/uima" "$UIMA_HOME" -ignorecase
+"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/examples" .wsdd "C:/Program Files/apache/uima" "$UIMA_HOME" -ignorecase
+"$JAVA_HOME/bin/java" -cp "$UIMA_HOME/lib/uima_core.jar" org.apache.uima.internal.util.ReplaceStringInFiles "$UIMA_HOME/examples" .xml "C:/Temp" temp -ignorecase
