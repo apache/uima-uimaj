80d191e1d4b2f8db29aebce0ca0853bab10bb7aa
Adam P. Lally <alally@apache.org>
2006-11-22 21:31:29 +0000

    UIMA-63:  Created a utility for copying FS from one CAS to another.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@478333 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uimaj-core/src/test/java/org/apache/uima/util/CasCopierTest.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.uima.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import junit.framework.TestCase;
+
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.XCASDeserializer;
+import org.apache.uima.cas.impl.XmiCasSerializer;
+import org.apache.uima.cas_data.impl.CasComparer;
+import org.apache.uima.resource.metadata.FsIndexDescription;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.apache.uima.resource.metadata.impl.TypePriorities_impl;
+import org.apache.uima.test.junit_extension.JUnitExtension;
+
+/**
+ * 
+ */
+public class CasCopierTest extends TestCase
+{
+  private TypeSystemDescription typeSystem;
+  private FsIndexDescription[] indexes; 
+
+  protected void setUp() throws Exception
+  {
+    File typeSystemFile = JUnitExtension.getFile("ExampleCas/testTypeSystem.xml");
+    File indexesFile = JUnitExtension.getFile("ExampleCas/kltIndexes.xml");
+
+    typeSystem = UIMAFramework.getXMLParser().parseTypeSystemDescription(
+            new XMLInputSource(typeSystemFile));
+    indexes = UIMAFramework.getXMLParser().parseFsIndexCollection(
+            new XMLInputSource(indexesFile)).getFsIndexes();
+  }
+
+  public void testCopyCas() throws Exception
+  {
+    //create a source CAS by deserializing from XCAS
+    CAS srcCas = CasCreationUtils.createCas(typeSystem, new TypePriorities_impl(), indexes);
+    InputStream serCasStream = new FileInputStream(JUnitExtension.getFile("ExampleCas/cas.xml"));
+    XCASDeserializer.deserialize(serCasStream, srcCas);
+    serCasStream.close();
+    
+    //create a destination CAS
+    CAS destCas = CasCreationUtils.createCas(typeSystem, new TypePriorities_impl(), indexes);
+    
+    //do the copy
+    CasCopier.copyCasView(srcCas, destCas, true);
+    XmiCasSerializer.serialize(destCas, System.out);
+    
+    //verify copy
+    CasComparer.assertEquals(srcCas, destCas);
+  }
+  
+  public void testCopyFs() throws Exception
+  {
+    //create a source CAS by deserializing from XCAS
+    CAS srcCas = CasCreationUtils.createCas(typeSystem, new TypePriorities_impl(), indexes);
+    InputStream serCasStream = new FileInputStream(JUnitExtension.getFile("ExampleCas/cas.xml"));
+    XCASDeserializer.deserialize(serCasStream, srcCas);
+    serCasStream.close();
+    
+    //create a destination CAS and the CasCopier instance
+    CAS destCas = CasCreationUtils.createCas(typeSystem, new TypePriorities_impl(), indexes);
+    CasCopier copier = new CasCopier(destCas);
+    
+    //set sofa data in destination CAS (this is not copied automtically)
+    destCas.setDocumentText(srcCas.getDocumentText());
+    
+    //copy an Annotation
+    Iterator annotIter = srcCas.getAnnotationIndex().iterator();
+    FeatureStructure annot = (FeatureStructure)annotIter.next();
+    FeatureStructure copy = copier.copyFs(annot);
+    //verify copy
+    CasComparer.assertEquals(annot, copy);
+    
+    //copy a Relation (which will have references)
+    Iterator relationIter = srcCas.getIndexRepository().getIndex("kltRelationIndex").iterator();
+    FeatureStructure relFS = (FeatureStructure)relationIter.next();
+    FeatureStructure relCopy = copier.copyFs(relFS);
+    //verify copy
+    CasComparer.assertEquals(relFS, relCopy);    
+  }
+}
