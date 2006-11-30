e581c04128b309159a01851ffb599b487f3b6254
Adam P. Lally <alally@apache.org>
2006-11-30 20:34:12 +0000

    UIMA-28: RunAE package name changed. https://issues.apache.org/jira/browse/UIMA-28
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@481062 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-distr/src/main/scripts/runAE.sh
+++ uimaj-distr/src/main/scripts/runAE.sh
@@ -4,4 +4,4 @@ if [ "$JAVA_HOME" = "" ];
 then
   JAVA_HOME=$UIMA_HOME/java/jre
 fi
-"$JAVA_HOME/bin/java" -cp "$UIMA_CLASSPATH" "-Duima.datapath=$UIMA_DATAPATH" -DVNS_HOST=$VNS_HOST -DVNS_PORT=$VNS_PORT "-Djava.util.logging.config.file=$UIMA_HOME/Logger.properties" -Xms128M -Xmx256M org.apache.uima.examples.RunAE $*
+"$JAVA_HOME/bin/java" -cp "$UIMA_CLASSPATH" "-Duima.datapath=$UIMA_DATAPATH" -DVNS_HOST=$VNS_HOST -DVNS_PORT=$VNS_PORT "-Djava.util.logging.config.file=$UIMA_HOME/Logger.properties" -Xms128M -Xmx256M org.apache.uima.tools.RunAE $*
