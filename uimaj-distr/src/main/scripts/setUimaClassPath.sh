43efe141295ae6cbf1a246c19b0180bfa7471a99
Thilo Goetz <twgoetz@apache.org>
2006-12-07 14:56:48 +0000

    Jira issue UIMA-103: add license headers to script files
    https://issues.apache.org/jira/browse/UIMA-103
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@483475 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-distr/src/main/scripts/setUimaClassPath.sh
+++ uimaj-distr/src/main/scripts/setUimaClassPath.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+
+#   Licensed to the Apache Software Foundation (ASF) under one
+#   or more contributor license agreements.  See the NOTICE file
+#   distributed with this work for additional information
+#   regarding copyright ownership.  The ASF licenses this file
+#   to you under the Apache License, Version 2.0 (the
+#   "License"); you may not use this file except in compliance
+#   with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing,
+#   software distributed under the License is distributed on an
+#   #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#   KIND, either express or implied.  See the License for the
+#   specific language governing permissions and limitations
+#   under the License.
+
 UIMA_CLASSPATH=$UIMA_CLASSPATH:$UIMA_HOME/docs/examples/resources:$UIMA_HOME/lib/uima-core.jar:$UIMA_HOME/lib/uima-cpe.jar:$UIMA_HOME/lib/uima-tools.jar:$UIMA_HOME/lib/uima-examples.jar:$UIMA_HOME/lib/uima-adapter-soap.jar:$UIMA_HOME/lib/uima-adapter-vinci.jar:$CATALINA_HOME/webapps/axis/WEB-INF/lib/axis.jar:$CATALINA_HOME/webapps/axis/WEB-INF/lib/commons-discovery.jar:$CATALINA_HOME/webapps/axis/WEB-INF/lib/commons-discovery-0.2.jar:$CATALINA_HOME/webapps/axis/WEB-INF/lib/commons-logging.jar:$CATALINA_HOME/webapps/axis/WEB-INF/lib/commons-logging-1.0.4.jar:$CATALINA_HOME/webapps/axis/WEB-INF/lib/jaxrpc.jar:$CATALINA_HOME/webapps/axis/WEB-INF/lib/saaj.jar:$CATALINA_HOME/webapps/axis/WEB-INF/lib/activation.jar:$UIMA_HOME/lib/jVinci.jar:$CLASSPATH
 #also set default values for VNS_HOST and VNS_PORT
 if [ "$VNS_HOST" = "" ];
