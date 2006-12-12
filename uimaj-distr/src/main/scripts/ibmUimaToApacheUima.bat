48372c3566dc6cb01aa7b5cc6344cb764bfae679
Adam P. Lally <alally@apache.org>
2006-12-12 21:57:47 +0000

    Added utility that updates import statements replacing IBM UIMA package names with Apache UIMA package names. UIMA-49: https://issues.apache.org/jira/browse/UIMA-49
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@486348 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uimaj-distr/src/main/scripts/ibmUimaToApacheUima.bat
@@ -0,0 +1,27 @@
+@echo off
+
+REM   Licensed to the Apache Software Foundation (ASF) under one
+REM   or more contributor license agreements.  See the NOTICE file
+REM   distributed with this work for additional information
+REM   regarding copyright ownership.  The ASF licenses this file
+REM   to you under the Apache License, Version 2.0 (the
+REM   "License"); you may not use this file except in compliance
+REM   with the License.  You may obtain a copy of the License at
+REM
+REM    http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM   Unless required by applicable law or agreed to in writing,
+REM   software distributed under the License is distributed on an
+REM   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+REM   KIND, either express or implied.  See the License for the
+REM   specific language governing permissions and limitations
+REM   under the License.
+
+@echo on
+
+if "%JAVA_HOME%"=="" set JAVA_HOME=%UIMA_HOME%\java\jre
+REM   update package names in import statements
+"%JAVA_HOME%\bin\java" -cp "%UIMA_HOME%/lib/uima-core.jar;%UIMA_HOME%/lib/uima-tools.jar" org.apache.uima.tools.migration.IbmToApachePackageNames %1
+REM   update frameworkImplementation elements in XML descriptors
+"%JAVA_HOME%\bin\java" -cp "%UIMA_HOME%/lib/uima-core.jar" org.apache.uima.internal.util.ReplaceStringInFiles %1 .xml com.ibm.uima.java org.apache.uima.java
+"%JAVA_HOME%\bin\java" -cp "%UIMA_HOME%/lib/uima-core.jar" org.apache.uima.internal.util.ReplaceStringInFiles %1 .xml JEDII org.apache.uima.java
