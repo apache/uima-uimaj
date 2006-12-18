9b3b40783885afdc5c9213314027640ceed61437
Thilo Goetz <twgoetz@apache.org>
2006-12-18 17:16:51 +0000

    Jira UIMA-128: string subtype values not checked on feature assignment.
    https://issues.apache.org/jira/browse/UIMA-128
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@488346 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/test/java/org/apache/uima/cas/test/StringSubtypeTest.java
+++ uimaj-core/src/test/java/org/apache/uima/cas/test/StringSubtypeTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.uima.cas.test;
 
 import java.io.File;
@@ -8,7 +27,11 @@ import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.ResultSpecification;
 import org.apache.uima.analysis_engine.annotator.JTextAnnotator_ImplBase;
+import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASRuntimeException;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.Type;
 import org.apache.uima.cas.TypeSystem;
 import org.apache.uima.cas.impl.LowLevelCAS;
 import org.apache.uima.cas.impl.LowLevelTypeSystem;
@@ -91,6 +114,25 @@ public class StringSubtypeTest extends TestCase {
     boolean exCaught = false;
     try {
       cas.ll_setStringValue(addr, stringSetFeat, undefinedValue);
+    } catch (CASRuntimeException e) {
+      exCaught = true;
+    }
+    assertTrue(exCaught);
+  }
+
+  public void testCas() {
+    CAS cas = this.jcas.getCas();
+    TypeSystem ts = cas.getTypeSystem();
+    Type annotType = ts.getType(annotationTypeName);
+    FeatureStructure fs = cas.createFS(annotType);
+    Feature stringSetFeat = ts.getFeatureByFullName(annotationTypeName
+	+ TypeSystem.FEATURE_SEPARATOR + stringSetFeatureName);
+    fs.setStringValue(stringSetFeat, definedValue1);
+    fs.setStringValue(stringSetFeat, definedValue2);
+    fs.setStringValue(stringSetFeat, definedValue3);
+    boolean exCaught = false;
+    try {
+      fs.setStringValue(stringSetFeat, undefinedValue);
     } catch (CASRuntimeException e) {
       exCaught = true;
     }
