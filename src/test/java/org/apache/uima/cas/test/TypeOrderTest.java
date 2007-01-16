1c12be1203e17e04d7dd1b65aa7bebfcffcba3b0
Thilo Goetz <twgoetz@apache.org>
2007-01-16 14:29:58 +0000

    Clean up code prior to working on UIMA-197.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk/uimaj-core@496705 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/uima/cas/test/TypeOrderTest.java
+++ src/test/java/org/apache/uima/cas/test/TypeOrderTest.java
@@ -30,7 +30,6 @@ import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.cas.FSIndex;
 import org.apache.uima.cas.FSIterator;
-import org.apache.uima.cas.Feature;
 import org.apache.uima.cas.Type;
 import org.apache.uima.cas.TypeSystem;
 import org.apache.uima.cas.admin.CASFactory;
@@ -78,21 +77,9 @@ public class TypeOrderTest extends TestCase {
 
   private Type annotationType;
 
-  private Type wordType;
-
-  private Type separatorType;
-
-  private Type eosType;
-
   private Type tokenType;
 
-  private Feature tokenTypeFeature;
-
   private Type sentenceType;
-
-  private Feature startFeature;
-
-  private Feature endFeature;
 
   public TypeOrderTest(String arg) {
     super(arg);
@@ -103,39 +90,27 @@ public class TypeOrderTest extends TestCase {
    */
   protected void setUp() throws Exception {
     super.setUp();
-    casMgr = initCAS();
-    cas = casMgr.getCAS().getCurrentView();
+    this.casMgr = initCAS();
+    this.cas = this.casMgr.getCAS().getCurrentView();
 
-    TypeSystem ts = cas.getTypeSystem();
-    wordType = ts.getType(WORD_TYPE);
+    TypeSystem ts = this.cas.getTypeSystem();
     // assert(wordType != null);
-    separatorType = ts.getType(SEP_TYPE);
-    eosType = ts.getType(EOS_TYPE);
-    tokenType = ts.getType(TOKEN_TYPE);
-    tokenTypeFeature = ts.getFeatureByFullName(TOKEN_TYPE_FEAT_Q);
-    startFeature = ts.getFeatureByFullName(CAS.FEATURE_FULL_NAME_BEGIN);
-    endFeature = ts.getFeatureByFullName(CAS.FEATURE_FULL_NAME_END);
-    sentenceType = ts.getType(SENT_TYPE);
-    annotationType = ts.getType(CAS.TYPE_NAME_ANNOTATION);
-    assertTrue(annotationType != null);
+    this.tokenType = ts.getType(TOKEN_TYPE);
+    this.sentenceType = ts.getType(SENT_TYPE);
+    this.annotationType = ts.getType(CAS.TYPE_NAME_ANNOTATION);
+    assertTrue(this.annotationType != null);
   }
 
   public void tearDown() {
-    casMgr = null;
-    cas = null;
-    wordType = null;
-    separatorType = null;
-    eosType = null;
-    tokenType = null;
-    tokenTypeFeature = null;
-    startFeature = null;
-    endFeature = null;
-    sentenceType = null;
-    annotationType = null;
+    this.casMgr = null;
+    this.cas = null;
+    this.tokenType = null;
+    this.sentenceType = null;
+    this.annotationType = null;
   }
 
   // Initialize the first CAS.
-  private static CASMgr initCAS() throws CASException {
+  private static CASMgr initCAS() {
     // Create a CASMgr. Ensures existence of AnnotationFS type.
     CASMgr cas = CASFactory.createCAS();
     // Create a writable type system.
@@ -232,21 +207,21 @@ public class TypeOrderTest extends TestCase {
     Assert.assertNotNull(fileWriter);   
     
     for (int i = 0; i < 10; i++) {
-      cas.getIndexRepository().addFS(cas.createAnnotation(annotationType, i * 2, (i * 2) + 1));
-      cas.getIndexRepository().addFS(cas.createAnnotation(sentenceType, i * 2, (i * 2) + 1));
-      cas.getIndexRepository().addFS(cas.createAnnotation(tokenType, i * 2, (i * 2) + 1));
-      cas.getIndexRepository().addFS(cas.createAnnotation(tokenType, i * 2, (i * 2) + 1));
-      cas.getIndexRepository().addFS(cas.createAnnotation(tokenType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.annotationType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.sentenceType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.tokenType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.tokenType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.tokenType, i * 2, (i * 2) + 1));
     }
     for (int i = 19; i >= 10; i--) {
-      cas.getIndexRepository().addFS(cas.createAnnotation(annotationType, i * 2, (i * 2) + 1));
-      cas.getIndexRepository().addFS(cas.createAnnotation(sentenceType, i * 2, (i * 2) + 1));
-      cas.getIndexRepository().addFS(cas.createAnnotation(tokenType, i * 2, (i * 2) + 1));
-      cas.getIndexRepository().addFS(cas.createAnnotation(tokenType, i * 2, (i * 2) + 1));
-      cas.getIndexRepository().addFS(cas.createAnnotation(tokenType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.annotationType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.sentenceType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.tokenType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.tokenType, i * 2, (i * 2) + 1));
+      this.cas.getIndexRepository().addFS(this.cas.createAnnotation(this.tokenType, i * 2, (i * 2) + 1));
     }
 
-    FSIterator it = cas.getIndexRepository().getIndex(TYPE_ORDER_INDEX).iterator();
+    FSIterator it = this.cas.getIndexRepository().getIndex(TYPE_ORDER_INDEX).iterator();
 
     // it = cas.getAnnotationIndex().iterator();
     AnnotationFS fs;
@@ -261,7 +236,9 @@ public class TypeOrderTest extends TestCase {
     }
     
     fileWriter.close();
-    Assert.assertTrue(FileCompare.compare(refFile, outputFile));
+    System.out.println(refFile.getAbsolutePath());
+    System.out.println(outputFile.getAbsolutePath());
+    Assert.assertTrue("Comparing ref " + refFile.getAbsolutePath() + " and output " + outputFile.getAbsolutePath(), FileCompare.compare(refFile, outputFile));
   }
 
   public static void main(String[] args) {
