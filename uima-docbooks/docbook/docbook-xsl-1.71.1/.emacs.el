d5cb22771fdd7ab1100c0819b7e7f1d6c2a7254b
Marshall Schor <schor@apache.org>
2007-01-27 04:19:14 +0000

    UIMA-236 - undoing previous commit - should only commit the zip files to follow the same conventions as velocity project
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@500475 13f79535-47bb-0310-9956-ffa450edef68

--- uima-docbooks/docbook/docbook-xsl-1.71.1/.emacs.el
+++
@@ -1,6 +0,0 @@
-(add-hook
-  'nxml-mode-hook
-  (lambda ()
-    (setq rng-schema-locating-files-default
-          (append '("/sandbox/docbook/trunk/xsl/locatingrules.xml")
-                  rng-schema-locating-files-default ))))
