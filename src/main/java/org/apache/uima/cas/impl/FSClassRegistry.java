418fcad0ad41e81f00bfec62a964088398e9adff
Marshall Schor <schor@apache.org>
2007-06-06 21:29:51 +0000

    no Jira - add more info to error message without using Java 6 methods ...
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk/uimaj-core@544964 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/uima/cas/impl/FSClassRegistry.java
+++ src/main/java/org/apache/uima/cas/impl/FSClassRegistry.java
@@ -117,7 +117,7 @@ public class FSClassRegistry {
       CASRuntimeException casEx = new CASRuntimeException(CASRuntimeException.JCAS_CAS_MISMATCH,
               new String[] { (null == jcasView)? "-- ignore outer msg, error is can''t get value of jcas from cas"
                         : (jcasView.getType(type).casType.getName() + "; exception= " 
-                                + e.getClass().getSimpleName() + "; msg= " + e.getLocalizedMessage())});
+                                + e.getClass().getName() + "; msg= " + e.getLocalizedMessage())});
       casEx.initCause(e);
       throw casEx;      
     }
