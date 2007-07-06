ba30e91f8a8b432c62e85d1755f844fd204a22b7
Marshall Schor <schor@apache.org>
2007-07-06 14:08:04 +0000

    [UIMA-476] add test case with a type having a feature whose range is an array of a specific other type, then doing complete-serialization as is done with SOAP, and deserializing back - and verifying the type is ok
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@553884 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/test/java/org/apache/uima/cas/test/TypeSystemReinitTest.java
+++ uimaj-core/src/test/java/org/apache/uima/cas/test/TypeSystemReinitTest.java
@@ -27,10 +27,14 @@ import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
 import org.apache.uima.cas.admin.CASMgr;
 import org.apache.uima.cas.impl.CASCompleteSerializer;
 import org.apache.uima.cas.impl.CASImpl;
 import org.apache.uima.cas.impl.Serialization;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.impl.TypeSystemImpl;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.apache.uima.resource.metadata.impl.TypeSystemDescription_impl;
 import org.apache.uima.test.junit_extension.JUnitExtension;
@@ -70,6 +74,40 @@ public class TypeSystemReinitTest extends TestCase {
 
       FeatureStructure fs = tcas3.createFS(tcas3.getTypeSystem().getType("NamedEntity"));
       tcas3.getIndexRepository().addFS(fs);
+    } catch (Exception e) {
+      JUnitExtension.handleException(e);
+    }
+  }
+  public void testReinitCASCompleteSerializerWithArrays() throws Exception {
+    try {
+      AnalysisEngineDescription aed = UIMAFramework.getXMLParser().parseAnalysisEngineDescription(
+              new XMLInputSource(JUnitExtension
+                      .getFile("ExampleTae/arrayTypeSerialization.xml")));
+     
+      CAS cas1 = CasCreationUtils.createCas(aed);
+      cas1.setDocumentText("foo");
+      CASCompleteSerializer ser = Serialization.serializeCASComplete((CASMgr) cas1);
+
+      CAS tcas2 = CasCreationUtils.createCas(new TypeSystemDescription_impl(), null, null);
+      CASImpl cas2 = ((CASImpl) tcas2).getBaseCAS();
+      tcas2.setDocumentText("bar");
+
+      // reinit
+      //  This uses cas2 which only has a base type system to start, 
+      //    and loads it from a complete serialization which has other new types
+      cas2.reinit(ser);
+      CAS tcas3 = cas2.getCurrentView();
+
+      assertTrue(tcas2 == tcas3);
+      assertNotNull(cas1.getTypeSystem().getType("Test.ArrayType"));
+      assertNotNull(tcas3.getTypeSystem().getType("Test.ArrayType"));
+      
+      TypeSystemImpl ts = (TypeSystemImpl)cas2.getTypeSystem();
+      Type arrayType = ts.getType("Test.ArrayType");
+      TypeImpl featRange = (TypeImpl)arrayType.getFeatureByBaseName("arrayFeature").getRange();
+     
+      assertTrue(ts.ll_isArrayType(featRange.getCode()));
+      
     } catch (Exception e) {
       JUnitExtension.handleException(e);
     }
