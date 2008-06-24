801adde7044fe7e954b366886fbb3e7557b14cd4
Michael Baessler <mbaessler@apache.org>
2008-06-24 09:13:05 +0000

    UIMA-1024
    update version number to 2.3.0-incubating-SNAPSHOT
    
    https://issues.apache.org/jira/browse/UIMA-1024
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk/uimaj-core@671078 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/uima/impl/UIMAFramework_impl.java
+++ src/main/java/org/apache/uima/impl/UIMAFramework_impl.java
@@ -192,14 +192,14 @@ public class UIMAFramework_impl extends UIMAFramework {
    * @see org.apache.uima.UIMAFramework#_getMinorVersion()
    */
   public short _getMinorVersion() {
-    return 2; // minor version
+    return 3; // minor version
   }
 
   /**
    * @see org.apache.uima.UIMAFramework#_getBuildRevision()
    */
   public short _getBuildRevision() {
-    return 2; // build revision
+    return 0; // build revision
   }
 
   /**
