187ff630ec44935eda798dc8cd940ea8d01476c7
Jaroslaw Cwiklik <cwiklik@apache.org>
2009-09-08 15:33:20 +0000

    UIMA-1560 Removed unused vars: primitiveTAE and debug
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@812557 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-cpe/src/test/java/org/apache/uima/analysis_engine/impl/SofaNamingInAggregateTest.java
+++ uimaj-cpe/src/test/java/org/apache/uima/analysis_engine/impl/SofaNamingInAggregateTest.java
@@ -47,10 +47,6 @@ public class SofaNamingInAggregateTest extends TestCase {
 
   PrimitiveAnalysisEngine_impl delegateAE;
 
-  AnalysisEngine primitiveTAE;
-
-  boolean debug = true;
-
   private AnalysisEngineDescription aeDescriptor;
 
   private FlowControllerContainer flowController;
