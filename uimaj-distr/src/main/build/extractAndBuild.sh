48b5f017ff2b2e46e6b11a21eca903c078843022
Marshall Schor <schor@apache.org>
2009-09-28 22:43:12 +0000

    UIMA-1590 fix linux scripts - copy of docbook tooling, match windows build approach.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk@819759 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-distr/src/main/build/extractAndBuild.sh
+++ uimaj-distr/src/main/build/extractAndBuild.sh
@@ -20,15 +20,18 @@
 # Bourne shell syntax, this should hopefully run on pretty much anything.
 
 usage() {
-  echo "Usage: extractAndBuild.sh <level> [-notest] [-deploy]"
-  echo "           (-notest and -deploy cannot be used together)"
+  echo "Run this command in a directory where the files will be extracted to."
+  echo "Usage: extractAndBuild.sh <level> <release-candidate> [-notest] [-deploy]"
+  echo "          (-notest and -deploy cannot be used together)"
+  echo "examples of the 1st 2 arguments, level release-candidate, are  trunk trunk   or  2.2.2  01"
+  echo "If trunk, use the word \"trunk\" for the 2nd argument, e.g. extractAndBuild.bat trunk trunk"
 }
 
-vmargs=""
-mvnCommand=install
+jvmargs=""
+mvnCommand="clean install"
 
 # Check arguments
-if [ $# = 0 ]
+if [ $# -eq 0 -o $# gt 3 ]
 then
   usage
   exit 1
@@ -36,39 +39,39 @@ fi
 
 if [ "$1" = "trunk" ]
 then
-  level=trunk
+  svnloc=trunk
   leveldir=trunk
 else
-  level=tags/$1
-  leveldir=$1
+  leveldir="uimaj-$1-$2"
+  svnloc="tags/uimaj-$1/$leveldir"
 fi
 
-if [ $# -gt "2" ]
-then
-  usage
-  exit 1
-fi
-
-if [ -n "$2" ]
+if [ -n "$3" ]
 then
 # Check for -notest switch.  If present, add the no-test define to the mvn command line.
-  if [ "$2" = "-notest" ]
+  if [ "$3" = "-notest" ]
   then
-    vmargs="-Dmaven.test.skip=true"
+    jvmargs="-Dmaven.test.skip=true"
 # Check for -deploy switch.  If present, change maven command to deploy artifacts to remote Maven repo
-  elif [ "$2" = "-deploy" ]
+  elif [ "$3" = "-deploy" ]
   then
-    vmargs="-DsignArtifacts=true"
-    mvnCommand="source:jar deploy"
+    jvmargs="-DsignArtifacts=true"
+    mvnCommand="deploy"
   else
     usage
     exit 1
   fi
 fi
 
-svn checkout http://svn.apache.org/repos/asf/incubator/uima/uimaj/$level
-cd $leveldir/uimaj
-mvn ${vmargs} -Duima.build.date="`date`" $mvnCommand
+svn checkout -r HEAD http://svn.apache.org/repos/asf/incubator/uima/uimaj/$svnloc
+cd $leveldir
+cp -r ${0%/*}/../../../../uima-docbook-tool/tools/fop-versions/fop-0.95             uima-docbook-tool/tools/fop-versions 
+cp -r ${0%/*}/../../../../uima-docbook-tool/tools/jai-versions/jai-1.1.3            uima-docbook-tool/tools/jai-versions 
+cp -r ${0%/*}/../../../../uima-docbook-tool/tools/docbook-versions/docbook-xml-4.5  uima-docbook-tool/tools/docbook-versions
+cp -r ${0%/*}/../../../../uima-docbook-tool/tools/docbook-versions/docbook-xsl-1.72.0  uima-docbook-tool/tools/docbook-versions 
+cp -r ${0%/*}/../../../../uima-docbook-tool/tools/saxon-versions/saxon-6.5.5           uima-docbook-tool/tools/saxon-versions
+cd uimaj
+mvn ${jvmargs} -Duima.build.date="`date`" $mvnCommand
 cd ..
 cd uimaj-distr
 mvn assembly:assembly
