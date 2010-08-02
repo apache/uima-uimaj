9b522fe6c5c8552ab3c69e5b8387b5ee8a5f44e6
Marshall Schor <schor@apache.org>
2010-08-02 20:15:26 +0000

    [UIMA-1834] added a new class, UimaVersion and UimaAsVersion, which is not in the source-path, and has substitutable values for the version (major, minor, incremental), and updated the pom to use filtered resource processing to produce a new class under target which has the version info from the pom substituted in.
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/trunk/uimaj-core@981669 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/versionJava/org/apache/uima/impl/UimaVersion.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.uima.impl;
+
+public class UimaVersion {
+  /**
+   * @see org.apache.uima.UIMAFramework#_getMajorVersion()
+   */
+  public static short getMajorVersion() {
+    return ${parsedVersion.majorVersion}; // major version
+  }
+
+  /**
+   * @see org.apache.uima.UIMAFramework#_getMinorVersion()
+   */
+  public static short getMinorVersion() {
+    return ${parsedVersion.minorVersion}; // minor version
+  }
+
+  /**
+   * @see org.apache.uima.UIMAFramework#_getBuildRevision()
+   */
+  public static short getBuildRevision() {
+    return ${parsedVersion.incrementalVersion}; // build revision
+  }
+
+}
