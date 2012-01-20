94ca3882551fbee2abfd7dccb1a1255fc2dbe4a7
Marshall Schor <schor@apache.org>
2012-01-20 04:03:53 +0000

    [UIMA-2350] make operational properties creation from xml have proper defaults
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/trunk@1233752 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/main/java/org/apache/uima/resource/metadata/impl/OperationalProperties_impl.java
+++ uimaj-core/src/main/java/org/apache/uima/resource/metadata/impl/OperationalProperties_impl.java
@@ -20,6 +20,11 @@
 package org.apache.uima.resource.metadata.impl;
 
 import org.apache.uima.resource.metadata.OperationalProperties;
+import org.apache.uima.util.InvalidXMLException;
+import org.apache.uima.util.XMLParser;
+import org.apache.uima.util.XMLParser.ParsingOptions;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 public class OperationalProperties_impl extends MetaDataObject_impl implements
         OperationalProperties {
@@ -85,4 +90,29 @@ public class OperationalProperties_impl extends MetaDataObject_impl implements
           new PropertyXmlInfo[] { new PropertyXmlInfo("modifiesCas"),
               new PropertyXmlInfo("multipleDeploymentAllowed"),
               new PropertyXmlInfo("outputsNewCASes") });
+
+  /* (non-Javadoc)
+   * @see org.apache.uima.resource.metadata.impl.MetaDataObject_impl#buildFromXMLElement(org.w3c.dom.Element, org.apache.uima.util.XMLParser, org.apache.uima.util.XMLParser.ParsingOptions)
+   */
+  @Override
+  public void buildFromXMLElement(Element aElement, XMLParser aParser, ParsingOptions aOptions)
+      throws InvalidXMLException {
+    // overridden to set proper defaults
+    // https://issues.apache.org/jira/browse/UIMA-2350
+    // These change depending on the element which is the parent of the parent of this element
+    String grandParentName = "";
+    Node p = aElement.getParentNode();
+    if (p instanceof Element) {
+      Node gp = p.getParentNode();
+      if (gp instanceof Element) {
+        grandParentName = ((Element) gp).getTagName();
+      }
+    }
+    setModifiesCas(grandParentName.equals("casConsumerDescription") ? false : true);
+    setOutputsNewCASes(grandParentName.equals("collectionReaderDescription") ? true : false);
+    setMultipleDeploymentAllowed(grandParentName.equals("collectionReaderDescription") ||
+                                 grandParentName.equals("casConsumerDescription") ? false : true);   
+    super.buildFromXMLElement(aElement, aParser, aOptions);
+  }
 }
+
