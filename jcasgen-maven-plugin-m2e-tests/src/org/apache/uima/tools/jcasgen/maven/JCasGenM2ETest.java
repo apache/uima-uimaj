8a8f082a920af283435d0b0a8e3c424ca39f1d3a
Richard Eckart de Castilho <rec@apache.org>
2013-05-30 16:48:57 +0000

    [UIMA-2471] add a jcasgen-maven-plugin (and m2e connector) - Removed BSD references from POMs with permission from Steven (see UIMA-2471) - Added Apache headers as usual
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/trunk@1487927 13f79535-47bb-0310-9956-ffa450edef68

--- jcasgen-maven-plugin-m2e-tests/src/org/apache/uima/tools/jcasgen/maven/JCasGenM2ETest.java
+++ jcasgen-maven-plugin-m2e-tests/src/org/apache/uima/tools/jcasgen/maven/JCasGenM2ETest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.uima.tools.jcasgen.maven;
 
 import java.util.HashSet;
@@ -10,7 +28,6 @@ import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.m2e.core.project.ResolverConfiguration;
-import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
 
 @SuppressWarnings("restriction")
 public class JCasGenM2ETest extends AbstractMavenProjectTestCase {
--- jcasgen-maven-plugin/src/main/java/org/apache/uima/tools/jcasgen/maven/JCasGenMojo.java
+++ jcasgen-maven-plugin/src/main/java/org/apache/uima/tools/jcasgen/maven/JCasGenMojo.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.uima.tools.jcasgen.maven;
 
 import java.io.File;
@@ -111,13 +129,8 @@ public class JCasGenMojo extends AbstractMojo {
 
     // run JCasGen to generate the Java sources
     Jg jCasGen = new Jg();
-    String[] args = new String[] {
-        "-jcasgeninput",
-        typeSystemFile.toString(),
-        "-jcasgenoutput",
-        this.outputDirectory.getAbsolutePath(),
-        "=jcasgenclasspath",
-        classpath };
+    String[] args = new String[] { "-jcasgeninput", typeSystemFile.toString(), "-jcasgenoutput",
+        this.outputDirectory.getAbsolutePath(), "=jcasgenclasspath", classpath };
     try {
       jCasGen.main0(args, null, new JCasGenProgressMonitor(), new JCasGenErrors());
     } catch (JCasGenException e) {
@@ -221,9 +234,8 @@ public class JCasGenMojo extends AbstractMojo {
         String targetPath = resource.getTargetPath();
         for (String filePath : scanner.getIncludedFiles()) {
           File sourceFile = new File(resourceDir, filePath);
-          File baseDirectory = targetPath != null
-              ? new File(buildOutputDirectory, targetPath)
-              : buildOutputDirectory;
+          File baseDirectory = targetPath != null ? new File(buildOutputDirectory, targetPath)
+                  : buildOutputDirectory;
           File targetFile = new File(baseDirectory, filePath);
           targetToSource.put(targetFile, sourceFile);
         }
