332d4558f8095465b1e1021658b1500c813a9ecf
Marshall Schor <schor@apache.org>
2014-04-14 20:59:42 +0000

    [UIMA-2383] comment change only - fix Javadoc description of AnnotationBase
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/trunk/uimaj-core@1587318 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/uima/jcas/cas/AnnotationBase.java
+++ src/main/java/org/apache/uima/jcas/cas/AnnotationBase.java
@@ -26,9 +26,22 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.JCasRegistry;
 
 /**
- * the JCas class model for the CAS type uima.cas.Annotation. It defines two integer valued features
- * indicating the begin and end of the span being annotated. There is also a method to retrieve the
- * spanned text as a string.
+ * the JCas class model for the CAS type uima.cas.AnnotationBase. 
+ * The AnnotationBase type defines one system-used feature which 
+ * specifies for an annotation the subject of analysis (Sofa) to which it refers. 
+ * Various annotation types (including the built-in uima.tcas.Annotation)
+ * may be defined as subtypes of this type.
+ * 
+ * uima.tcas.Annotation is a subtype of this type, appropriate for
+ * Subjects of Analysis which are text strings.  Other (not-built-in)
+ * subtypes may be defined for other kinds of Subjects of Analysis.  For instance
+ * an audio sample Subject of Analysis might define a start and end position as time points 
+ * in the stream.  An image Subject of Analysis might define rectangular coordiantes
+ * describing a sub-area of the image.
+ * 
+ * If you are defining a type which needs a reference to the Subject of Analysis
+ * (which is view-specific),
+ * it should be a subtype of this base type.
  */
 public class AnnotationBase extends org.apache.uima.jcas.cas.TOP implements AnnotationBaseFS {
 
