a1759c2dc4dd2faa3cf9be0cd1bff20cd0d4efb9
Marshall Schor <schor@apache.org>
2014-04-15 18:35:48 +0000

    [UIMA-2101] add slightly reworked patch.  changes include reverting casts in getFeatureAttributes for array types (they started out as (StringArrayFS) but the patch had (StringArrayFSImpl), which fails if the Cas passed in has had its JCas version requested. Merged the test case with the existing test case.  The asserts were slightly modified to reflect the fact that the formatted version doesn't insert a new-line before the first <Document> tag. Also improved existing test case to test the result of the operation (not just print it out)
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/trunk/uimaj-core@1587675 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/uima/util/CasToInlineXml.java
+++ src/main/java/org/apache/uima/util/CasToInlineXml.java
@@ -20,7 +20,6 @@
 package org.apache.uima.util;
 
 import java.io.ByteArrayOutputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,12 +45,12 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
 
 /**
- * Generates an *approximate* inline XML representation of a CAS. 
+ * Generates an *approximate* inline XML representation of a CAS.
  * Annotation types are represented as XML tags, features are represented as attributes.
- *  
+ * 
  * Features whose values are FeatureStructures are not represented.
  * Feature values which are strings longer than 64 characters are truncated.
- * Feature values which are arrays of primitives are represented by 
+ * Feature values which are arrays of primitives are represented by
  * strings that look like [ xxx, xxx ]
  * 
  * The Subject of analysis is presumed to be a text string.
@@ -59,11 +58,18 @@ import org.xml.sax.helpers.AttributesImpl;
  * Some characters in the document's Subject-of-analysis
  * are replaced by blanks, because the characters aren't valid in xml documents.
  * 
- * It doesn't work for annotations which are overlapping, because these cannot 
+ * It doesn't work for annotations which are overlapping, because these cannot
  * be properly represented as properly - nested XML.
  * 
+ * To use this, make an instance of this class, and
+ * (optionally) set the formattedOutput to true or false.
+ * 
+ * Then call one of the public methods to format or generate the Inline XML.
  */
 public class CasToInlineXml {
+
+  private boolean formattedOutput = true;
+
   /**
    * This destroy method does nothing.
    * 
@@ -104,19 +110,17 @@ public class CasToInlineXml {
   }
 
   /**
-   * Generates inline XML from a CAS.
+   * Generates inline XML from a CAS using a passed in ContentHandler
    * 
    * @param aCAS
    *          CAS to generate from
    * @param aFilter
    *          constraint that determines which annotations are included in the output. If null (or
    *          omitted), all annotations are included.
-   * @return the inline XML version of the CAS
-   * @throws CASException -
+   * @param aHandler the content handler to use
+   * @throws CASException
    */
-  public String generateXML(CAS aCAS, FSMatchConstraint aFilter) throws CASException {
-    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    XMLSerializer sax2xml = new XMLSerializer(byteArrayOutputStream);
+  public void generateXML(CAS aCAS, FSMatchConstraint aFilter, ContentHandler aHandler) throws CASException {
 
     // get document text
     String docText = aCAS.getDocumentText();
@@ -141,10 +145,9 @@ public class CasToInlineXml {
     int pos = 0;
 
     try {
-      ContentHandler handler = sax2xml.getContentHandler();
-      handler.startDocument();
+      aHandler.startDocument();
       // write an artificial start tag
-      handler.startElement("", "Document", "Document", new AttributesImpl());
+      aHandler.startElement("", "Document", "Document", new AttributesImpl());
       // now use null is a placeholder for this artificial Document annotation
       AnnotationFS curAnnot = null;
 
@@ -154,7 +157,7 @@ public class CasToInlineXml {
         // System.out.println("Type: " + fs.getType().getName() + ", Class:" +
         // fs.getClass().getName());
         // AnnotationFS nextAnnot = (AnnotationFS)fs;
-        AnnotationFS nextAnnot = (AnnotationFS) iterator.get();
+        AnnotationFS nextAnnot = iterator.get();
 
         if (curAnnot == null || nextAnnot.getBegin() < curAnnot.getEnd()) {
           // nextAnnot's start point is within the span of curAnnot
@@ -164,10 +167,10 @@ public class CasToInlineXml {
 
             // write text between current pos and beginning of nextAnnot
             try {
-              handler.characters(docCharArray, pos, nextAnnot.getBegin() - pos);
+              aHandler.characters(docCharArray, pos, nextAnnot.getBegin() - pos);
               pos = nextAnnot.getBegin();
-              handler.startElement("", nextAnnot.getType().getName(),
-                      nextAnnot.getType().getName(), getFeatureAttributes(nextAnnot, aCAS));
+              aHandler.startElement("", nextAnnot.getType().getName(),
+                  nextAnnot.getType().getName(), getFeatureAttributes(nextAnnot, aCAS));
 
               // push parent annotation on stack
               stack.add(curAnnot);
@@ -175,7 +178,7 @@ public class CasToInlineXml {
               curAnnot = nextAnnot;
             } catch (StringIndexOutOfBoundsException e) {
               System.err.println("Invalid annotation range: " + nextAnnot.getBegin() + ","
-                      + nextAnnot.getEnd() + " in document of length " + docText.length());
+                  + nextAnnot.getEnd() + " in document of length " + docText.length());
             }
           }
           iterator.moveToNext();
@@ -183,69 +186,83 @@ public class CasToInlineXml {
           // nextAnnot begins after curAnnot ends
           // write text between current pos and end of curAnnot
           try {
-            handler.characters(docCharArray, pos, curAnnot.getEnd() - pos);
+            aHandler.characters(docCharArray, pos, curAnnot.getEnd() - pos);
             pos = curAnnot.getEnd();
           } catch (StringIndexOutOfBoundsException e) {
             System.err.println("Invalid annotation range: " + curAnnot.getBegin() + ","
-                    + curAnnot.getEnd() + " in document of length " + docText.length());
+                + curAnnot.getEnd() + " in document of length " + docText.length());
           }
-          handler.endElement("", curAnnot.getType().getName(), curAnnot.getType().getName());
+          aHandler.endElement("", curAnnot.getType().getName(), curAnnot.getType().getName());
 
           // pop next containing annotation off stack
-          curAnnot = (AnnotationFS) stack.remove(stack.size() - 1);
+          curAnnot = stack.remove(stack.size() - 1);
         }
       }
 
       // finished writing all start tags, now finish up
       if (curAnnot != null) {
         try {
-          handler.characters(docCharArray, pos, curAnnot.getEnd() - pos);
+          aHandler.characters(docCharArray, pos, curAnnot.getEnd() - pos);
           pos = curAnnot.getEnd();
         } catch (StringIndexOutOfBoundsException e) {
           System.err.println("Invalid annotation range: " + curAnnot.getBegin() + ","
-                  + curAnnot.getEnd() + "in document of length " + docText.length());
+              + curAnnot.getEnd() + "in document of length " + docText.length());
         }
-        handler.endElement("", curAnnot.getType().getName(), curAnnot.getType().getName());
+        aHandler.endElement("", curAnnot.getType().getName(), curAnnot.getType().getName());
 
         while (!stack.isEmpty()) {
-          curAnnot = (AnnotationFS) stack.remove(stack.size() - 1); // pop
+          curAnnot = stack.remove(stack.size() - 1); // pop
           if (curAnnot == null) {
             break;
           }
           try {
-            handler.characters(docCharArray, pos, curAnnot.getEnd() - pos);
+            aHandler.characters(docCharArray, pos, curAnnot.getEnd() - pos);
             pos = curAnnot.getEnd();
           } catch (StringIndexOutOfBoundsException e) {
             System.err.println("Invalid annotation range: " + curAnnot.getBegin() + ","
-                    + curAnnot.getEnd() + "in document of length " + docText.length());
+                + curAnnot.getEnd() + "in document of length " + docText.length());
           }
-          handler.endElement("", curAnnot.getType().getName(), curAnnot.getType().getName());
+          aHandler.endElement("", curAnnot.getType().getName(), curAnnot.getType().getName());
         }
       }
 
       if (pos < docCharArray.length) {
-        handler.characters(docCharArray, pos, docCharArray.length - pos);
+        aHandler.characters(docCharArray, pos, docCharArray.length - pos);
       }
-      handler.endElement("", "Document", "Document");
-      handler.endDocument();
+      aHandler.endElement("", "Document", "Document");
+      aHandler.endDocument();
 
-      // return XML string
-      return new String(byteArrayOutputStream.toByteArray(),"UTF-8");
     } catch (SAXException e) {
-      throw new UIMARuntimeException(e);
-    } catch (UnsupportedEncodingException e) {
       throw new UIMARuntimeException(e);
     }
   }
 
-  private final Attributes getFeatureAttributes(FeatureStructure aFS, CAS aCAS) {
+
+  /**
+   * Generates inline XML from a CAS.
+   * 
+   * @param aCAS
+   *          CAS to generate from
+   * @param aFilter
+   *          constraint that determines which annotations are included in the output. If null (or
+   *          ommitted), all annotations are included.
+   * @ return the inline form
+   */
+  public String generateXML(CAS aCAS, FSMatchConstraint aFilter) throws CASException {
+    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    XMLSerializer sax2xml = new XMLSerializer(byteArrayOutputStream, formattedOutput);
+    generateXML(aCAS, aFilter, sax2xml.getContentHandler());
+    // return XML string
+    return new String(byteArrayOutputStream.toByteArray());
+  }
+
+  private Attributes getFeatureAttributes(FeatureStructure aFS, CAS aCAS) {
     AttributesImpl attrs = new AttributesImpl();
     for (Feature feat : aFS.getType().getFeatures()) {
       String featName = feat.getShortName();
       // how we get feature value depends on feature's range type)
       String rangeTypeName = feat.getRange().getName();
-      if (feat.getRange().isPrimitive())
-      {
+      if (feat.getRange().isPrimitive()) {
         String str = aFS.getFeatureValueAsString(feat);
         if (str == null) {
           attrs.addAttribute("", featName, featName, "CDATA", "null");
@@ -255,43 +272,50 @@ public class CasToInlineXml {
           }
           attrs.addAttribute("", featName, featName, "CDATA", str);
         }
-      } else if (feat.getRange().isArray() && feat.getRange().getComponentType().isPrimitive())
-      {
-        //TODO: there should be a better way to get any array value as a string array
+      } else if (feat.getRange().isArray() && feat.getRange().getComponentType().isPrimitive()) {
+        // TODO: there should be a better way to get any array value as a string array
         String[] vals = null;
         if (CAS.TYPE_NAME_STRING_ARRAY.equals(rangeTypeName)) {
           StringArrayFS arrayFS = (StringArrayFS) aFS.getFeatureValue(feat);
-          if (arrayFS != null)
+          if (arrayFS != null) {
             vals = arrayFS.toArray();
+          }
         } else if (CAS.TYPE_NAME_INTEGER_ARRAY.equals(rangeTypeName)) {
           IntArrayFS arrayFS = (IntArrayFS) aFS.getFeatureValue(feat);
-          if (arrayFS != null)
+          if (arrayFS != null) {
             vals = arrayFS.toStringArray();
+          }
         } else if (CAS.TYPE_NAME_FLOAT_ARRAY.equals(rangeTypeName)) {
           FloatArrayFS arrayFS = (FloatArrayFS) aFS.getFeatureValue(feat);
-          if (arrayFS != null)
+          if (arrayFS != null) {
             vals = arrayFS.toStringArray();
+          }
         } else if (CAS.TYPE_NAME_BOOLEAN_ARRAY.equals(rangeTypeName)) {
           BooleanArrayFS arrayFS = (BooleanArrayFS) aFS.getFeatureValue(feat);
-          if (arrayFS != null)
+          if (arrayFS != null) {
             vals = arrayFS.toStringArray();
+          }
         } else if (CAS.TYPE_NAME_BYTE_ARRAY.equals(rangeTypeName)) {
           ByteArrayFS arrayFS = (ByteArrayFS) aFS.getFeatureValue(feat);
-          if (arrayFS != null)
+          if (arrayFS != null) {
             vals = arrayFS.toStringArray();
+          }
         } else if (CAS.TYPE_NAME_SHORT_ARRAY.equals(rangeTypeName)) {
           ShortArrayFS arrayFS = (ShortArrayFS) aFS.getFeatureValue(feat);
-          if (arrayFS != null)
+          if (arrayFS != null) {
             vals = arrayFS.toStringArray();
+          }
         } else if (CAS.TYPE_NAME_LONG_ARRAY.equals(rangeTypeName)) {
           LongArrayFS arrayFS = (LongArrayFS) aFS.getFeatureValue(feat);
-          if (arrayFS != null)
+          if (arrayFS != null) {
             vals = arrayFS.toStringArray();
+          }
         } else if (CAS.TYPE_NAME_DOUBLE_ARRAY.equals(rangeTypeName)) {
           DoubleArrayFS arrayFS = (DoubleArrayFS) aFS.getFeatureValue(feat);
-          if (arrayFS != null)
+          if (arrayFS != null) {
             vals = arrayFS.toStringArray();
-        } 
+          }
+        }
         String attrVal;
         if (vals == null) {
           attrVal = "null";
@@ -308,7 +332,7 @@ public class CasToInlineXml {
           buf.append(']');
           attrVal = buf.toString();
         }
-        attrs.addAttribute("", featName, featName, "CDATA", attrVal);        
+        attrs.addAttribute("", featName, featName, "CDATA", attrVal);
       } else {
         // get value as FeatureStructure
         FeatureStructure fsVal = aFS.getFeatureValue(feat);
@@ -338,12 +362,30 @@ public class CasToInlineXml {
   private void replaceInvalidXmlChars(char[] aChars) {
     for (int i = 0; i < aChars.length; i++) {
       if ((aChars[i] < 0x20 && aChars[i] != 0x09 && aChars[i] != 0x0A && aChars[i] != 0x0D)
-              || (aChars[i] > 0xD7FF && aChars[i] < 0xE000) || aChars[i] == 0xFFFE
-              || aChars[i] == 0xFFFF) {
+          || (aChars[i] > 0xD7FF && aChars[i] < 0xE000) || aChars[i] == 0xFFFE
+          || aChars[i] == 0xFFFF) {
         // System.out.println("Found invalid XML character: " + (int)aChars[i] + " at position " +
         // i); //temp
         aChars[i] = ' ';
       }
     }
   }
+
+
+  /**
+   * @return true if the output will be formatted
+   */
+  public boolean isFormattedOutput() {
+    return formattedOutput;
+  }
+  
+  /**
+   * Set a flag that will be used to control how the ContentHandler
+   * will be initialized - to either format or not, the generated Inline XML
+   * @param formattedOutput true means to format the output, and is the default
+   */
+  public void setFormattedOutput(boolean formattedOutput) {
+    this.formattedOutput = formattedOutput;
+  }
+
 }
