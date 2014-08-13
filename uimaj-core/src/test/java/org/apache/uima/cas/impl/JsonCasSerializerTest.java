c20362716d2c702cb6a5df00d3b564de391a3eee
Marshall Schor <schor@apache.org>
2014-08-13 17:21:03 +0000

    [UIMA-3969] start testing, fix one bug when both @context and view info are omitted.
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/trunk@1617770 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/test/java/org/apache/uima/cas/impl/JsonCasSerializerTest.java
+++ uimaj-core/src/test/java/org/apache/uima/cas/impl/JsonCasSerializerTest.java
@@ -1,11 +1,37 @@
 package org.apache.uima.cas.impl;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+
 import junit.framework.TestCase;
+
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.cas.impl.XmiCasSerializer.JsonContextFormat;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.apache.uima.test.junit_extension.JUnitExtension;
+import org.apache.uima.util.CasCreationUtils;
+import org.apache.uima.util.FileUtils;
+import org.apache.uima.util.InvalidXMLException;
+import org.apache.uima.util.XMLInputSource;
+import org.apache.uima.util.XMLParser;
+import org.xml.sax.SAXException;
 
 public class JsonCasSerializerTest extends TestCase {
 
+  private XMLParser parser = UIMAFramework.getXMLParser();
+  private TypeSystemDescription tsd;
+  private CASImpl cas;
+  private TypeSystemImpl tsi;
+  private TypeImpl topType;
+  private XmiCasSerializer xcs;
+  private String expectedResults;
+  private TypeImpl annotationType;
+  
   protected void setUp() throws Exception {
     super.setUp();
+    xcs = new XmiCasSerializer(null);
   }
 
   protected void tearDown() throws Exception {
@@ -31,15 +57,72 @@ public class JsonCasSerializerTest extends TestCase {
    *   boolean:  prettyprint, marker/delta, @context, views, 
    *             (without both context and views), byId/byType, errorHandler/noErrorHandler,
    *             JsonFactory/noJsonFactory, 
+   *             typeFiltering
+   * @throws SAXException 
    *     @context: expanded-names, supertypes, fsrefs
    *   configuring using Jackson: JsonFactory, JsonGenerator
    *   views: 0, 1 or 2 views
-   *   for delta: 0 / non-0 modifications
+   *   for delta: 0 / non-0 modifications   
    *                          above the line
    *                          below the line
+   *                        mods with/without indexing update
    *   namespaces: with/without collision
+   *   
+   *   File, Writer, OutputStream
    */
 
+  public void testBasic() throws Exception {
+    setupTypeSystem("nameSpaceNeeded.xml");
+    cas.addFsToIndexes(cas.createFS(topType));
+    String r = serialize();
+    assertEquals(getExpected("top.txt"), r);
+    cas.reset();
+    cas = (CASImpl) cas.createView("basicView");
+    cas.addFsToIndexes(cas.createFS(annotationType));
+    r = serialize();
+    assertEquals(getExpected("topWithNamedView.txt"), r);
+    cas.reset();
+    cas = (CASImpl) cas.getCurrentView(); // default view
+    cas.addFsToIndexes(cas.createFS(annotationType));
+    r = serialize();
+    assertEquals(getExpected("topWithDefaultView.txt"), r);
+    
+    cas.reset();
+    xcs.setJsonContext(JsonContextFormat.omitContext);
+    cas.addFsToIndexes(cas.createFS(topType));
+    r = serialize();
+    assertEquals(getExpected("topNoContext.txt"), r);
+    
+    cas.reset();
+    xcs.setJsonContext(JsonContextFormat.omitContext);
+    xcs.setCasViews(false);
+    cas.addFsToIndexes(cas.createFS(topType));
+    r = serialize();
+    assertEquals(getExpected("topNoContextNoViews.txt"), r);
+    
+  }
+  
+  
+  private void setupTypeSystem(String tsdName) throws InvalidXMLException, IOException, ResourceInitializationException {
+    File tsdFile = JUnitExtension.getFile("CasSerialization/desc/" + tsdName);
+    tsd = parser.parseTypeSystemDescription(new XMLInputSource(tsdFile));
+    cas = (CASImpl) CasCreationUtils.createCas(tsd, null, null);
+    tsi = cas.getTypeSystemImpl();
+    topType = (TypeImpl) tsi.getTopType();
+    annotationType = tsi.annotType; 
+  }
+  
+  private String getExpected(String expectedResultsName) throws IOException {
+    File expectedResultsFile = JUnitExtension.getFile("CasSerialization/expected/" + expectedResultsName);
+    return expectedResults = FileUtils.file2String(expectedResultsFile, "utf-8");
+  }
+  
+  private String serialize() throws SAXException {    
+    StringWriter sw = new StringWriter();
+    xcs.serializeJson(cas, sw);
+    return sw.toString();
+  }
+  
   public void testJsonSerializeCASObject() {
   }
 
