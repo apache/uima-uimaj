0314e077bb6db150c495cd16483ceb05811f782a
Marshall Schor <schor@apache.org>
2015-12-01 12:05:18 +0000

    [UIMA-4673] make the merged features a linked hashmap so iteration preserves feature order.  Makes some test cases work (e.g. cas2inlineXml), and is better for pretty printing, and is better for backwards compatibility.
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/branches/experiment-v3-jcas@1717402 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/main/java/org/apache/uima/cas/impl/TypeImpl.java
+++ uimaj-core/src/main/java/org/apache/uima/cas/impl/TypeImpl.java
@@ -24,6 +24,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
@@ -113,7 +114,7 @@ public class TypeImpl implements Type, Comparable<TypeImpl> {
   private final List<TypeImpl> directSubtypes = new ArrayList<>();
     
   // ********  Features  *********
-  private final Map<String, FeatureImpl> staticMergedFeatures = new HashMap<String, FeatureImpl>(1);
+  private final Map<String, FeatureImpl> staticMergedFeatures = new LinkedHashMap<>(1);
   private final List<FeatureImpl> staticMergedFeaturesIntroducedByThisType = new ArrayList<>(1);
   
   /**
