e21433f57118616aa898e4b2df048ec130348e53
Marshall Schor <schor@apache.org>
2016-01-04 01:55:33 +0000

    [UIMA-4674] add tests for next higher power of x
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/branches/experiment-v3-jcas@1722783 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uimaj-core/src/test/java/org/apache/uima/util/MiscTest.java
@@ -0,0 +1,23 @@
+package org.apache.uima.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MiscTest {
+
+  @Test
+  public void test() {
+    assertEquals(8, Misc.nextHigherPowerOfX(0, 8));
+    assertEquals(8, Misc.nextHigherPowerOfX(-0, 8));
+    assertEquals(8, Misc.nextHigherPowerOfX(1, 8));
+    assertEquals(8, Misc.nextHigherPowerOfX(7, 8));
+    assertEquals(8, Misc.nextHigherPowerOfX(8, 8));
+    assertEquals(16, Misc.nextHigherPowerOfX(9, 8));
+    System.out.println(Misc.nextHigherPowerOfX(10 * 1024 * 1024 * 8 / 3 / 50, 4096)); // == 561152
+    System.out.println( Misc.nextHigherPowerOfX(Math.max(512, 561152/1000), 32));
+  
+
+  }
+
+}
