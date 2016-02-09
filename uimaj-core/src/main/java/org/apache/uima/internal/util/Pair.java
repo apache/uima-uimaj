cbf8b61cda2df8628953da0ef1aab4bf551183b4
Marshall Schor <schor@apache.org>
2016-02-09 21:48:57 +0000

    [UIMA-4674] add generic Pair (of T, U) with hashcode and equals for use as key, used in compare method for CASes to break recursions
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/branches/experiment-v3-jcas@1729468 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uimaj-core/src/main/java/org/apache/uima/internal/util/Pair.java
@@ -0,0 +1,51 @@
+package org.apache.uima.internal.util;
+
+public class Pair<T, U> {
+
+  final T t;
+  final U u;
+
+  public Pair(T t, U u) {
+    this.t = t;
+    this.u = u;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((t == null) ? 0 : t.hashCode());
+    result = prime * result + ((u == null) ? 0 : u.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Pair other = (Pair) obj;
+    if (t == null) {
+      if (other.t != null) {
+        return false;
+      }
+    } else if (!t.equals(other.t)) {
+      return false;
+    }
+    if (u == null) {
+      if (other.u != null) {
+        return false;
+      }
+    } else if (!u.equals(other.u)) {
+      return false;
+    }
+    return true;
+  }
+  
+}
