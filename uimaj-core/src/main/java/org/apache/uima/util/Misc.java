3411108a36cc393d40bd0ddd3b2d6b0d76a15572
Marshall Schor <schor@apache.org>
2016-04-15 14:31:41 +0000

    [UIMA-4897] Add elide and caller trace methods
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/trunk@1739305 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/main/java/org/apache/uima/util/Misc.java
+++ uimaj-core/src/main/java/org/apache/uima/util/Misc.java
@@ -19,7 +19,70 @@
 
 package org.apache.uima.util;
 
+import java.util.regex.Pattern;
+
 public class Misc {
+
+  private static final Pattern whitespace = Pattern.compile("\\s");
+
+  public static String replaceWhiteSpace(String s, String replacement) {
+    return whitespace.matcher(s).replaceAll(replacement);
+  }
+  
+  
+  /**
+   * @param s starting frames above invoker
+   * @param n max number of callers to return
+   * @return  x called by: y ...
+   */
+  public static StringBuilder getCallers(final int s, final int n) {
+    StackTraceElement[] e = Thread.currentThread().getStackTrace();
+    StringBuilder sb = new StringBuilder();
+    for (int i = s + 2; i < s + n + 3; i++) {
+      if (i >= e.length) break;
+      if (i != s + 2) {
+        sb.append("  called_by: ");
+      }
+      sb.append(formatcaller(e[i]));      
+    }
+    return sb;
+  }
+
+  /**
+   * @return the name of the caller in the stack
+   */
+  public static String getCaller() {
+    StackTraceElement[] e = Thread.currentThread().getStackTrace();
+    return formatcaller(e[4]) + "called by: " + formatcaller(e[5]);
+  }
+  
+  private static String formatcaller(StackTraceElement e) {
+    String n = e.getClassName();
+    return n.substring(1 + n.lastIndexOf('.')) + "." + e.getMethodName() + "[" + e.getLineNumber() + "]";
+  }
+  
+
+  
+  public static final String blanks = "                                                     ";
+  public static final String dots = "...";
+  
+  public static String elide(String s, int n) {
+    return elide(s, n, true);
+  }
+  
+  public static String elide(String s, int n, boolean pad) {
+    if (s == null) {
+      s = "null";
+    }
+    int sl = s.length();
+    if (sl <= n) {
+      return s + (pad ? blanks.substring(0, n - sl) : "");
+    }
+    int dl = 1; // (n < 11) ? 1 : (n < 29) ? 2 : 3;  // number of dots
+    int ss = (n - dl) / 2;
+    int ss2 = ss + (( ss * 2 == (n - dl)) ? 0 : 1);
+    return s.substring(0, ss) + dots.substring(0, dl) + s.substring(sl - ss2);
+  }  
 
   /**
    * 
@@ -33,6 +96,8 @@ public class Misc {
   }
   
 //  public static void main(String[] args) {
+//    System.out.println(elide("uninflectedWord", 11));
+//  }
 //    System.out.println("should be false - not defined: " + getNoValueSystemProperty("foo"));
 //    System.setProperty("foo", "");
 //    System.out.println("should be true - defined, 0 len str value: " + getNoValueSystemProperty("foo"));
