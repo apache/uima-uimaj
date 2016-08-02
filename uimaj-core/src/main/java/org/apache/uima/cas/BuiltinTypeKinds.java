8b5667a74ba274cfa1470cdf28450dfc023aaa70
Marshall Schor <schor@apache.org>
2016-08-02 13:57:54 +0000

    no Jira - slight refactoring, comment adding
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/branches/experiment-v3-jcas@1754926 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/main/java/org/apache/uima/cas/BuiltinTypeKinds.java
+++ uimaj-core/src/main/java/org/apache/uima/cas/BuiltinTypeKinds.java
@@ -5,13 +5,25 @@ import java.util.Set;
 
 import org.apache.uima.internal.util.Misc;
 
+/**
+ * Constants representing Built in type collections
+ *
+ * String Sets: 
+ * 
+ *     creatableArrays
+ *     primitiveTypeNames == noncreatable primitives
+ *     creatableBuiltinJcas (e.g. empty/non-empty FloatList
+ *     non creatable primitives (e.g. can't do createFS for primitive int)
+ *     
+ *     non creatable and builtin Arrays
+ */
 public class BuiltinTypeKinds {
   
   private static final Set<String> primitiveTypeNames = new HashSet<String>();
 
   public static final Set<String> creatableArrays = new HashSet<String>();
    
-  public static final Set<String> nonCreatablePrimitives = new HashSet<String>();
+  public static final Set<String> nonCreatablePrimitives = primitiveTypeNames;
 
   /**
    *  These types can not be created with CAS.createFS().
@@ -61,31 +73,18 @@ public class BuiltinTypeKinds {
         CAS.TYPE_NAME_NON_EMPTY_FLOAT_LIST,
         CAS.TYPE_NAME_NON_EMPTY_FS_LIST,
         CAS.TYPE_NAME_NON_EMPTY_INTEGER_LIST,
-        CAS.TYPE_NAME_NON_EMPTY_STRING_LIST
+        CAS.TYPE_NAME_NON_EMPTY_STRING_LIST,
+        CAS.TYPE_NAME_TOP,
+        CAS.TYPE_NAME_ANNOTATION_BASE,
+        CAS.TYPE_NAME_ANNOTATION      
     );    
+    creatableBuiltinJCas.addAll(creatableArrays);
  
-    Misc.addAll(nonCreatablePrimitives, 
-        CAS.TYPE_NAME_BOOLEAN,
-        CAS.TYPE_NAME_BYTE,
-        CAS.TYPE_NAME_SHORT,
-        CAS.TYPE_NAME_INTEGER,
-        CAS.TYPE_NAME_LONG,
-        CAS.TYPE_NAME_FLOAT,
-        CAS.TYPE_NAME_DOUBLE,
-        CAS.TYPE_NAME_STRING,
-        CAS.TYPE_NAME_JAVA_OBJECT
-        );
-
     nonCreatableTypesAndBuiltinArrays.addAll(nonCreatablePrimitives);
     nonCreatableTypesAndBuiltinArrays.addAll(creatableArrays);
     Misc.addAll(nonCreatableTypesAndBuiltinArrays, CAS.TYPE_NAME_SOFA);
     
-    creatableBuiltinJCas.addAll(creatableArrays);
-    Misc.addAll(creatableBuiltinJCas, 
-        CAS.TYPE_NAME_TOP,
-        CAS.TYPE_NAME_ANNOTATION_BASE,
-        CAS.TYPE_NAME_ANNOTATION      
-        );    
+       
   }
   
   /*****************  public getters and predicates *****************/
