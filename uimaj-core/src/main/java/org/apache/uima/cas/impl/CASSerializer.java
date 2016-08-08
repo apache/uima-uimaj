e46b5b213f7ec47958f12caeee7c7c7092808c99
Marshall Schor <schor@apache.org>
2016-08-08 15:26:33 +0000

    [UIMA-4743] add test case for delta binary serialization of mods to existing byte/short/long arrays. fix errors in CASSerializer. augment error message to reveal wrapped exception as part of message (was being ignored).
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimaj/trunk@1755512 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/main/java/org/apache/uima/cas/impl/CASSerializer.java
+++ uimaj-core/src/main/java/org/apache/uima/cas/impl/CASSerializer.java
@@ -68,12 +68,6 @@ public class CASSerializer implements Serializable {
 
   static final long serialVersionUID = -7972011651957420295L;
 
-  /**
-   * Set this to true to not fix the Jira 4743 bug where it causes more bytes to be written
-   *   (the part that doesn't cause a serialized byte format change is still fixed)
-   */
-  private boolean bkwdCompatJira4743 = true;
-  
   // The heap itself.
   public int[] heapArray = null;
 
@@ -250,7 +244,7 @@ public class CASSerializer implements Serializable {
 
       // output the key and version number
       CommonSerDes.createHeader()
-      .seqVer(bkwdCompatJira4743 ? 0 : 1)     // version 1 for UIMA-4743
+      .seqVer(1)     // not a delta, set version 1 for UIMA-4743 to inform receiver that we can handle version 1 format
       .write(dos);
      
       // output the FS heap
@@ -416,8 +410,8 @@ public class CASSerializer implements Serializable {
       this.fsIndex = cas.getDeltaIndexedFSs(mark);
       
       CommonSerDes.createHeader()
-      .delta()
-      .seqVer(bkwdCompatJira4743 ? 0 : 1)     // version 1 for UIMA-4743
+      .delta()                                // is delta
+      .seqVer(1)
        .write(dos);    
 
       // output the new FS heap cells
@@ -542,7 +536,7 @@ public class CASSerializer implements Serializable {
 	    }
 
       // word alignment
-      align = (4 - (byteheapsz % 4)) % 4;
+      align = (4 - (byteHeapModifiedAddrs.length % 4)) % 4;
       for (int i = 0; i < align; i++) {
         dos.writeByte(0);
       }
@@ -552,19 +546,16 @@ public class CASSerializer implements Serializable {
       short[] shortValues = new short[shortHeapModifiedAddrs.length];
       dos.writeInt(shortHeapModifiedAddrs.length);
       for (int i=0; i < shortHeapModifiedAddrs.length; i++) {
-        if (bkwdCompatJira4743) {
-      	  dos.writeShort(shortHeapModifiedAddrs[i]);
-        } else {
-          dos.writeInt(shortHeapModifiedAddrs[i]);
-        }
-      	shortValues[i] = cas.getShortHeap().getHeapValue(shortHeapModifiedAddrs[i]);
+        int modifiedAddr = shortHeapModifiedAddrs[i]; 
+        dos.writeInt(modifiedAddr);
+      	shortValues[i] = cas.getShortHeap().getHeapValue(modifiedAddr);
       }
       for (int i=0; i < shortValues.length; i++) {  
     	  dos.writeShort(cas.getShortHeap().getHeapValue(shortHeapModifiedAddrs[i]));
 	  }
       
       // word alignment
-      if (shortheapsz % 2 != 0) {
+      if (shortHeapModifiedAddrs.length % 2 != 0) {
         dos.writeShort(0);
       }
       
@@ -573,12 +564,9 @@ public class CASSerializer implements Serializable {
       long[] longValues = new long[longHeapModifiedAddrs.length];
       dos.writeInt(longHeapModifiedAddrs.length);
       for (int i=0; i < longHeapModifiedAddrs.length; i++) {
-        if (bkwdCompatJira4743) {
-          dos.writeShort(longHeapModifiedAddrs[i]);
-        } else {
-          dos.writeInt(longHeapModifiedAddrs[i]);
-        }
-        longValues[i] = cas.getLongHeap().getHeapValue(longHeapModifiedAddrs[i]);
+        int modifiedAddr = longHeapModifiedAddrs[i];
+        dos.writeInt(modifiedAddr);
+        longValues[i] = cas.getLongHeap().getHeapValue(modifiedAddr);
       }
       for (int i=0; i < longValues.length; i++) {  
     	  dos.writeLong(cas.getLongHeap().getHeapValue(longHeapModifiedAddrs[i]));
@@ -661,22 +649,6 @@ public class CASSerializer implements Serializable {
 
   long[] getLongArray() {
     return this.longHeapArray;
-  }
-
-  /**
-   * @return true if writing backward compatible delta 
-   */
-  public boolean isBkwdCompatJira4743() {
-    return bkwdCompatJira4743;
-  }
-
-  /**
-   * @param keepBackwardCompatible
-   *          set to true to write backward compatible delta, false to fix this bug set to false by
-   *          uima-as version 2.9.0, and other users of delta binary serialization to fix this bug
-   */
-  public void setBkwdCompatJira4743(boolean keepBackwardCompatible) {
-    this.bkwdCompatJira4743 = keepBackwardCompatible;
   }
 
 }
