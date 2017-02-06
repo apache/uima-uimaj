9dcb061a595448f8588691150f1a5fccbe5a9cd9
Richard Eckart de Castilho <rec@apache.org>
2017-02-06 14:00:30 +0000

    [UIMA-5299] - Work around UIMA-5097 in SimplePipeline unit test - Creating a CAS locally instead of re-using the shared one from the test base class solves the problem
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimafit/trunk@1781901 13f79535-47bb-0310-9956-ffa450edef68

--- uimafit-core/src/test/java/org/apache/uima/fit/util/SimplePipelineTest.java
+++ uimafit-core/src/test/java/org/apache/uima/fit/util/SimplePipelineTest.java
@@ -25,27 +25,39 @@ import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.fit.ComponentTestBase;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.TypePrioritiesFactory;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
 import org.apache.uima.fit.factory.testAes.Annotator1;
 import org.apache.uima.fit.factory.testAes.Annotator2;
 import org.apache.uima.fit.factory.testAes.Annotator3;
 import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.metadata.TypePriorities;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.apache.uima.util.CasCreationUtils;
 import org.junit.Test;
 
 /**
  */
-
 public class SimplePipelineTest extends ComponentTestBase {
 
   @Test
   public void test1() throws UIMAException, IOException {
-    CasIOUtil.readJCas(jCas, new File("src/test/resources/data/docs/test.xmi"));
+    // Creating a CAS locally here to work around UIMA-5097 - otherwise this test may fail if
+    // run in Eclipse or in other unit test setups where the same JVM is re-used for multiple tests.
+    TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription();
+    TypePriorities tp = TypePrioritiesFactory.createTypePriorities(new String[] {
+        "org.apache.uima.fit.type.Sentence", "org.apache.uima.fit.type.AnalyzedText",
+        "org.apache.uima.fit.type.Token" });
+    JCas jcas = CasCreationUtils.createCas(tsd, tp, null).getJCas();
+    
+    CasIOUtil.readJCas(jcas, new File("src/test/resources/data/docs/test.xmi"));
     AnalysisEngineDescription aed1 = AnalysisEngineFactory.createEngineDescription(
             Annotator1.class, typeSystemDescription);
     AnalysisEngineDescription aed2 = AnalysisEngineFactory.createEngineDescription(
             Annotator2.class, typeSystemDescription);
     AnalysisEngineDescription aed3 = AnalysisEngineFactory.createEngineDescription(
             Annotator3.class, typeSystemDescription);
-    SimplePipeline.runPipeline(jCas, aed1, aed2, aed3);
-
+    SimplePipeline.runPipeline(jcas, aed1, aed2, aed3);
   }
 }
