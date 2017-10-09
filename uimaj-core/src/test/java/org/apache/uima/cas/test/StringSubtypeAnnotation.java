77b1641fb316d478dc0a2e7ff8e8a802c6235648
Marshall Schor <schor@apache.org>
2017-10-09 00:09:12 +0000

    no Jira upgrade to latest jcas gen version output
    git-svn-id: https://svn.apache.org/repos/asf/uima/uv3/uimaj-v3/trunk@1811522 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/test/java/org/apache/uima/cas/test/StringSubtypeAnnotation.java
+++ uimaj-core/src/test/java/org/apache/uima/cas/test/StringSubtypeAnnotation.java
@@ -1,23 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 
    
-/* Apache UIMA v3 - First created by JCasGen Wed Mar 02 13:51:54 EST 2016 */
+/* Apache UIMA v3 - First created by JCasGen Sun Oct 08 20:01:40 EDT 2017 */
 
 package org.apache.uima.cas.test;
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.MethodHandle;
 
 import org.apache.uima.cas.impl.CASImpl;
 import org.apache.uima.cas.impl.TypeImpl;
 import org.apache.uima.cas.impl.TypeSystemImpl;
-import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCas; 
 import org.apache.uima.jcas.JCasRegistry;
+
+
 import org.apache.uima.jcas.tcas.Annotation;
 
 
 /** 
- * Updated by JCasGen Wed Mar 02 13:51:54 EST 2016
- * XML source: C:/au/svnCheckouts/branches/uimaj/experiment-v3-jcas/uimaj-core/src/test/resources/CASTests/desc/StringSubtypeTest.xml
+ * Updated by JCasGen Sun Oct 08 20:01:40 EDT 2017
+ * XML source: C:/au/svnCheckouts/uv3/trunk/uimaj-v3/uimaj-core/src/test/resources/CASTests/desc/StringSubtypeTest.xml
  * @generated */
 public class StringSubtypeAnnotation extends Annotation {
+ 
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static String _TypeName = "org.apache.uima.cas.test.StringSubtypeAnnotation";
+  
   /** @generated
    * @ordered 
    */
@@ -39,8 +69,12 @@ public class StringSubtypeAnnotation extends Annotation {
    *   Feature Offsets *
    * *******************/ 
    
+  public final static String _FeatName_stringSetFeature = "stringSetFeature";
+
+
   /* Feature Adjusted Offsets */
-  public final static int _FI_stringSetFeature = TypeSystemImpl.getAdjustedFeatureOffset("stringSetFeature");
+  private final static CallSite _FC_stringSetFeature = TypeSystemImpl.createCallSite(StringSubtypeAnnotation.class, "stringSetFeature");
+  private final static MethodHandle _FH_stringSetFeature = _FC_stringSetFeature.dynamicInvoker();
 
    
   /** Never called.  Disable default constructor
@@ -65,6 +99,19 @@ public class StringSubtypeAnnotation extends Annotation {
     readObject();   
   } 
 
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public StringSubtypeAnnotation(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
   /** 
    * <!-- begin-user-doc -->
    * Write your own initialization here
@@ -83,14 +130,14 @@ public class StringSubtypeAnnotation extends Annotation {
    * @generated
    * @return value of the feature 
    */
-  public String getStringSetFeature() { return _getStringValueNc(_FI_stringSetFeature);}
+  public String getStringSetFeature() { return _getStringValueNc(wrapGetIntCatchException(_FH_stringSetFeature));}
     
   /** setter for stringSetFeature - sets  
    * @generated
    * @param v value to set into the feature 
    */
   public void setStringSetFeature(String v) {
-    _setStringValueNfc(_FI_stringSetFeature, v);
+    _setStringValueNfc(wrapGetIntCatchException(_FH_stringSetFeature), v);
   }    
     
   }
