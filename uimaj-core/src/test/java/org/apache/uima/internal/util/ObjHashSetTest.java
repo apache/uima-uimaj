f8deadb4a96d5eb5b47d5516ef38548147decae6
Marshall Schor <schor@apache.org>
2017-12-15 15:40:08 +0000

    [UIMA-5674] multiple hash map upgrade using common code, fixing edge cases; add test cases including random ones
    git-svn-id: https://svn.apache.org/repos/asf/uima/uv3/uimaj-v3/trunk@1818296 13f79535-47bb-0310-9956-ffa450edef68

--- uimaj-core/src/test/java/org/apache/uima/internal/util/ObjHashSetTest.java
+++ uimaj-core/src/test/java/org/apache/uima/internal/util/ObjHashSetTest.java
@@ -25,75 +25,33 @@ import java.util.Random;
 import junit.framework.TestCase;
 
 
-public class IntHashSetTest extends TestCase {
+public class ObjHashSetTest extends TestCase {
   
-  IntHashSet ihs;
+  ObjHashSet<Integer> ihs;
   
   Random random;
   
   public void setUp() {
-    ihs = new IntHashSet();
+    ihs = new ObjHashSet<>(Integer.class, new Integer(Integer.MIN_VALUE));
   }
   
   public void testBasic() {
     
     ihs.add(15);
     ihs.add(188);
-    int[] sv = getSortedValues(ihs);
+    Integer[] sv = ihs.toArray();
     assertEquals(2, sv.length);
-    assertEquals(15, sv[0]);
-    assertEquals(188, sv[1]);
-    assertEquals(15, ihs.getMostNegative());
-    assertEquals(188, ihs.getMostPositive());
+    assertEquals(15 + 188, sv[0].intValue() + sv[1].intValue());
     
     // test most positive / negative
     ihs.clear();
     ihs.add(189);
-    assertEquals(189, ihs.getMostNegative());
-    assertEquals(189, ihs.getMostPositive());
     ihs.add(1000);
     ihs.add(-1000);
-    assertEquals(1000, ihs.getMostPositive());
-    assertEquals(-1000, ihs.getMostNegative());
     ihs.add(500);
     ihs.add(-500);
-    assertEquals(1000, ihs.getMostPositive());
-    assertEquals(-1000, ihs.getMostNegative());
     ihs.remove(1000);
-    assertEquals(999, ihs.getMostPositive());
-    assertEquals(-1000, ihs.getMostNegative());
     ihs.add(1001);    
-    assertEquals(1001, ihs.getMostPositive());
-    sv = getSortedValues(ihs);
-    assertTrue(Arrays.equals(sv, new int[]{-1000, -500, 189, 500, 1001}));
-  }
-  
-  public void testSwitching224() {
-    final int OS = 100000;
-    ihs = new IntHashSet(16, OS);
-    ihs.add(OS - 1);
-    ihs.add(OS);
-    ihs.add(OS + 1);
-    int[] sv = getSortedValues(ihs);
-    assertTrue(Arrays.equals(sv, new int[]{99999, 100000, 100001 }));
-    ihs.add(OS - 32767);
-    sv = getSortedValues(ihs);
-    assertTrue(Arrays.equals(sv, new int[]{OS - 32767, 99999, 100000, 100001}));
-    ihs.add(OS - 32768);
-    sv = getSortedValues(ihs);
-    assertTrue(Arrays.equals(sv, new int[]{OS - 32768, OS - 32767, 99999, 100000, 100001}));
-    
-  }
-  
-  private int[] getSortedValues(IntHashSet s) {
-    IntListIterator it = s.iterator();
-    int[] r = new int[s.size()];
-    int i = 0;
-    while (it.hasNext()) {
-      r[i++] = it.nextNvc();
-    }
-    Arrays.sort(r);
-    return r;
   }
   
   public void testContains() {
@@ -106,18 +64,11 @@ public class IntHashSetTest extends TestCase {
   }
   
 //  public void testTableSpace() {
-//    assertEquals(32, IntHashSet.tableSpace(19, 0.6f));    // 19 / .6 = 31.xxx, round to 32
-//    assertEquals(64, IntHashSet.tableSpace(21, 0.6f));
+//    assertEquals(32, ObjHashSet.tableSpace(19, 0.6f));    // 19 / .6 = 31.xxx, round to 32
+//    assertEquals(64, ObjHashSet.tableSpace(21, 0.6f));
 //    assertEquals(32, ihs.tableSpace(21));
 //  }
-  
-  public void testWontExpand() {
-    ihs = new IntHashSet(21);
-    assertEquals(16, ihs.getSpaceUsedInWords());
-    assertTrue(ihs.wontExpand(20));
-    assertFalse(ihs.wontExpand(21));
-  }
-  
+    
   public void testExpandNpe() {
     ihs.add(15);
     ihs.add(150000);  // makes 4 byte table entries
@@ -128,7 +79,7 @@ public class IntHashSetTest extends TestCase {
   }
   
   public void testAddIntoRemovedSlot() {
-    long seed = 6738591171221169418L;
+    long seed = -4571976104270514645L;
         new Random().nextLong();
     System.out.println("Random seed for testAddIntoRemovedSlot in " + this.getClass().getName() + ": "  + seed);
     random = new Random(seed);
@@ -143,7 +94,7 @@ public class IntHashSetTest extends TestCase {
     /** Test with 2 byte numbers */
     checkRemovedReuse(true);
     
-    ihs = new IntHashSet();
+    ihs = new ObjHashSet<>(Integer.class, new Integer(Integer.MIN_VALUE));
     for (int i = 1; i < 99; i++) {
       ihs.add(i);
     }
@@ -152,7 +103,6 @@ public class IntHashSetTest extends TestCase {
   }
   
   private void checkRemovedReuse(boolean is2) {
-    assertTrue(ihs.getSpaceUsedInWords() == ((is2) ? 128 : 256));
     for (int i = 0; i < 100000; i++) {
       int v = 1 + random.nextInt(100 + (i % 30000)); // random between 1 and 30,101
       int sz = ihs.size();
@@ -165,33 +115,24 @@ public class IntHashSetTest extends TestCase {
       assertEquals(sz + (wasAdded ? 1 : 0), ihs.size());
       assertTrue(ihs.contains(v));
     }
-    assertTrue(ihs.getSpaceUsedInWords() == ((is2) ? 16384 : 32768) );
-    
-    //  32,768, 16,384, 8,192, 4096, 2048, 1024, 512, 256
-    // for 2 byte storage, is2 = true, and expected is: i / 2
-    // for 4 byte storage, is2 = false, and expected is i
     
     ihs.clear(); // doesn't set 2nd time because size + removed > 1/2 the capacity
     
     for (int i = 32768; i > 128; i = i / 2) {
       ihs.clear(); // sets 2nd time shrinkable
-      assertTrue(ihs.getSpaceUsedInWords() == (is2 ? i/2 : i));
+      assertEquals(i, ihs.getCapacity());
       ihs.clear();  // shrinks
-      assertTrue(ihs.getSpaceUsedInWords() == (is2 ? i/4 : i/2));
+      assertEquals(i/2, ihs.getCapacity());
     }
 //    ihs.clear();
 //    
-    assertTrue(ihs.getSpaceUsedInWords() == (is2 ? 64: 128));
 
     // table size should be 128, adding 100 items should cause expansion (84 == .66 * 128)
-    for (int i = 1; i < ((is2) ? 100 : 99); i++) {
+    for (int i = 1; i < 100; i++) {
       ihs.add(i);
     }
-    if (!is2) {
-      ihs.add(100000);
-    }
     
-    assertTrue(ihs.getSpaceUsedInWords() == ((is2) ? 128 : 256));
+    assertEquals(256, ihs.getCapacity());
     for (int i = 0; i < 1000; i++) {
       int v = 1 + random.nextInt(100);
       ihs.remove(v);
@@ -200,7 +141,7 @@ public class IntHashSetTest extends TestCase {
       assertTrue(ihs.contains(v));
     }
     
-    assertTrue(ihs.getSpaceUsedInWords() == ((is2) ? 128 : 256));
+    assertEquals(256, ihs.getCapacity());
     
   }
  
