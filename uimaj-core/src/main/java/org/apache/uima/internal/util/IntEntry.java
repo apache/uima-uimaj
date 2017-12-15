85177725cde33dbe0feb8b9175245e0ad4350b00
Marshall Schor <schor@apache.org>
2017-12-15 15:36:07 +0000

    [UIMA-5674] upgrade to allow int hash maps to return iterators over entries
    git-svn-id: https://svn.apache.org/repos/asf/uima/uv3/uimaj-v3/trunk@1818294 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uimaj-core/src/main/java/org/apache/uima/internal/util/IntEntry.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.uima.internal.util;
+
+/**
+ * like Entry<k, v> except the k is an int. 
+ *
+ * @param <T> the type of the value
+ */
+public class IntEntry<T> {
+  private int i;
+  private T v;
+  IntEntry(int i, T v) {
+    this.i = i;
+    this.v = v;
+  }
+  
+  public int getKey() {
+    return i;
+  }
+  
+  public T getValue() {
+    return v;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + i;
+    result = prime * result + ((v == null) ? 0 : v.hashCode());
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    IntEntry other = (IntEntry) obj;
+    if (i != other.i)
+      return false;
+    if (v == null) {
+      if (other.v != null)
+        return false;
+    } else if (!v.equals(other.v))
+      return false;
+    return true;
+  }
+
+  
+  
+}
