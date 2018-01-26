d874c5fc1ef2c0c5560bc071289f5c310817e190
Marshall Schor <schor@apache.org>
2018-01-26 14:44:33 +0000

    no Jira update test pears to have uv3-beta versions of jcas files
    git-svn-id: https://svn.apache.org/repos/asf/uima/uv3/uimaj-v3/trunk@1822303 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uimaj-tools/src/test/resources/pearTests/pearMergerTests/uima.example.DateTime/src/org/apache/uima/tutorial/TimeAnnot.java
@@ -0,0 +1,92 @@
+
+
+   
+/* Apache UIMA v3 - First created by JCasGen Mon Aug 01 16:57:36 EDT 2016 */
+
+package org.apache.uima.tutorial;
+
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.impl.TypeSystemImpl;
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+
+
+
+
+/** 
+ * Updated by JCasGen Mon Aug 01 16:57:36 EDT 2016
+ * XML source: C:/temp/pear/pearTestSrc/DateTime/desc/TutorialTypeSystem.xml
+ * @generated */
+public class TimeAnnot extends DateTimeAnnot {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(TimeAnnot.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+ 
+  /* *******************
+   *   Feature Offsets *
+   * *******************/ 
+   
+  /* Feature Adjusted Offsets */
+
+   
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected TimeAnnot() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param casImpl the CAS this Feature Structure belongs to
+   * @param type the type of this Feature Structure 
+   */
+  public TimeAnnot(TypeImpl type, CASImpl casImpl) {
+    super(type, casImpl);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public TimeAnnot(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public TimeAnnot(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+}
+
+    
---
+++ uimaj-tools/src/test/resources/pearTests/pearMergerTests/uima.example.RoomNumber/src/org/apache/uima/tutorial/RoomNumber.java
@@ -0,0 +1,104 @@
+
+
+   
+/* Apache UIMA v3 - First created by JCasGen Mon Aug 01 10:52:25 EDT 2016 */
+
+package org.apache.uima.tutorial;
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.MethodHandle;
+
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.impl.TypeSystemImpl;
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Mon Aug 01 10:52:25 EDT 2016
+ * XML source: C:/temp/pear/pearTestSrc/RoomNumber/desc/TutorialTypeSystem.xml
+ * @generated */
+public class RoomNumber extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(RoomNumber.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+ 
+  /* *******************
+   *   Feature Offsets *
+   * *******************/ 
+   
+  /* Feature Adjusted Offsets */
+  private final static CallSite _FC_building = TypeSystemImpl.createCallSite(RoomNumber.class, "building");
+  private final static MethodHandle _FH_building = _FC_building.dynamicInvoker();
+
+   
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected RoomNumber() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param casImpl the CAS this Feature Structure belongs to
+   * @param type the type of this Feature Structure 
+   */
+  public RoomNumber(TypeImpl type, CASImpl casImpl) {
+    super(type, casImpl);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public RoomNumber(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: building
+
+  /** getter for building - gets Building containing this room
+   * @generated
+   * @return value of the feature 
+   */
+  public String getBuilding() { return _getStringValueNc(wrapGetIntCatchException(_FH_building));}
+    
+  /** setter for building - sets Building containing this room 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setBuilding(String v) {
+    _setStringValueNfc(wrapGetIntCatchException(_FH_building), v);
+  }    
+    
+  }
+
+    
